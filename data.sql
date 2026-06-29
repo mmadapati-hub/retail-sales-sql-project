0c2ec8be2591883c45605032e62b22105ffd474a
mmadapati-hub <mmveenaa@gmail.com>
2026-06-29 13:46:37 -0700

    Create data.sql
    Insert statements

---
+++ data.sql
@@ -0,0 +1,11 @@
+INSERT INTO customers VALUES
+(1, 'John Doe', 'New York', 'NY'),
+(2, 'Jane Smith', 'Texas', 'TX');
+
+INSERT INTO products VALUES
+(1, 'Laptop', 'Electronics', 800),
+(2, 'Phone', 'Electronics', 500);
+
+INSERT INTO orders VALUES
+(1, 1, 1, '2024-01-10', 2),
+(2, 2, 2, '2024-01-12', 1);
