dc47ae6969abb01f8a89aa8bb0b88b889cb6c53d
mmadapati-hub <mmveenaa@gmail.com>
2026-06-29 13:47:19 -0700

    analysis_queries.sql
    SELECT queries

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -1,1 +1,7 @@
 
+SELECT c.customer_name,
+       SUM(o.quantity * p.price) AS total_spent
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN products p ON o.product_id = p.product_id
+GROUP BY c.customer_name;
